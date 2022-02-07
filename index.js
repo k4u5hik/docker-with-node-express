bc1f7ac360e4fd634dc4cda5ad3e49d6319425c1
Kaushik Chemburkar <attherate@gmail.com>
2022-02-07 12:19:01 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h2>Hello World!</h2>');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+})
